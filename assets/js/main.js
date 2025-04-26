cf010482eed4e5605d3d55a862a1d135132f4e44
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2025-04-26 17:52:15 +0600

    mobile drawer menu completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,48 @@
 
 "use strict";
 /*=============================================
-	=    		Preloader			      =
+	=    		1. Preloader			      =
 =============================================*/
 window.addEventListener("load", function () {
   document.querySelector("body").classList.add("loaded");
 });
+
+/*=============================================
+	=    		2. Mobile Menu			      =
+=============================================*/
+// Offcanvas toggle
+document.querySelectorAll(".header-offcanvas-toogle").forEach((button) => {
+  button.addEventListener("click", () => {
+    document
+      .querySelector(".header-offcanvas")
+      ?.classList.add("header-offcanvas-open");
+    document
+      .querySelector(".header-offcanvas-overlay")
+      ?.classList.add("header-offcanvas-overlay-open");
+  });
+});
+
+document
+  .querySelectorAll(
+    ".header-offcanvas-close-toggle, .header-offcanvas-overlay, .header-click-close"
+  )
+  .forEach((item) => {
+    item.addEventListener("click", () => {
+      document
+        .querySelector(".header-offcanvas")
+        ?.classList.remove("header-offcanvas-open");
+      document
+        .querySelector(".header-offcanvas-overlay")
+        ?.classList.remove("header-offcanvas-overlay-open");
+    });
+  });
+
+// Mobile menu
+const tsMenuWrap = document
+  .querySelector(".header-mobile-menu-active > ul")
+  ?.cloneNode(true);
+const tsSideMenu = document.querySelector(".header-offcanvas-menu nav");
+
+if (tsMenuWrap && tsSideMenu) {
+  tsSideMenu.appendChild(tsMenuWrap);
+}
